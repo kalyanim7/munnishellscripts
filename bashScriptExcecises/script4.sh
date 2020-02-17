2624d6e0e8814e8bfe969ddf7228304eb6940dc3
kalyanim7 <60805003+kalyanim7@users.noreply.github.com>
2020-02-17 17:57:09 -0600

    numberscrpt2
    comments added after execution

--- bashScriptExcecises/script4.sh
+++ bashScriptExcecises/script4.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+# check the value if less than zero print value < 0
 if test $1 -lt 0
 then
         echo "$1 < 0"
 fi
- 
+ # check the value if less than or equal to print zero <= 0
 if test $1 -le 0
 then
         echo "$1 <= 0"
