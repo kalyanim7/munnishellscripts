d2f51e6438907f188f2c531555d66de5e5e1783a
kalyanim7 <60805003+kalyanim7@users.noreply.github.com>
2020-02-17 18:48:15 -0600

    numbercheck
    executed the script

--- bashScriptExcecises/script7.sh
+++ bashScriptExcecises/script7.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+# check the value greater than zero then print positive
 if [ $1 -gt 0 ]; then
   echo "$1 is positive"
+  # Check the value less than zero then print negative
 elif [ $1 -lt 0 ]
 then
   echo "$1 is negative"
+  # check the value is euqal to zero print zero else print its not a number 
 elif [ $1 -eq 0 ]
 then
   echo "$1 is zero"
